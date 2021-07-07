d8c5e1c2863063904e4ca653bfad954ded09a1b5
Abilat01 <Abilat01@yandex.ru>
2021-07-07 15:04:41 +0300

    Сохранение данных в coreData

--- ToDoListStudent/ToDoTableViewController.swift
+++ ToDoListStudent/ToDoTableViewController.swift
@@ -6,16 +6,82 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoTableViewController: UITableViewController {
     
-    @IBAction func saveTask(_ sender: UIBarButtonItem) {
+    var  tasks: [Tasks] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    //сохранение задачи в coreData
+    func saveTask(withTitle title: String) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        guard let entity = NSEntityDescription.entity(forEntityName: "Tasks", in: context) else { return }
+        
+        let taskObject = Tasks(entity: entity, insertInto: context)
+        taskObject.title = title
+        
+        do {
+            try context.save()
+            tasks.append(taskObject)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest: NSFetchRequest<Tasks> = Tasks.fetchRequest()
+        
+        do {
+            tasks = try context.fetch(fetchRequest)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        
+        return cell
+    }
+    
+    
+    //MARK: - IBACTION saveTask
+    
+    @IBAction func newCellTasks(_ sender: UIBarButtonItem) {
         
         let alertControler = UIAlertController(title: "Новая задача", message: "Пожалуйста, введите новую задачу", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Сохранить", style: .default) { action in
             let textField = alertControler.textFields?.first
             if let newTask = textField?.text {
-                self.tasks.insert(newTask, at: 0)
+                self.saveTask(withTitle: newTask)
                 self.tableView.reloadData()
             }
         }
@@ -29,35 +95,4 @@ class ToDoTableViewController: UITableViewController {
         present(alertControler, animated: true, completion: nil)
         
     }
-    
-    
-    var  tasks: [String] = []
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tasks.count
-    }
-
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        cell.textLabel?.text = tasks[indexPath.row]
-
-        return cell
-    }
-    
-
-    
 }
